dd8e731a2632a41b32e639591844d4481b7b2bb6
Development <development@Developments-MacBook-Pro.local>
2024-06-13 01:49:11 +0300

    Timing Error handling set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
-import { addUserAsync, loginUserAsync } from "@/services/userService";
 import Loader from "@/adapters/ui/components/customs/Loader";
 import LoginForm from "@/adapters/ui/components/LoginForm";
 import RegisterForm from "@/adapters/ui/components/RegisterForm";
@@ -14,16 +13,30 @@ import { UserEntity } from "@/domain/UserEntity";
 export default function Home() {
   const router = useRouter();
   const [showLoginForm, setShowLoginForm] = useState(false);
+  const [errorVisible, setErrorVisible] = useState(false);
   const userRepository = new FirebaseUserRepository();
-
   const dispatch = useDispatch();
   const { loading } = useSelector((state: RootState) => state.user);
   const error = useSelector((state: RootState) => state.user.error);
 
+  useEffect(() => {
+    if (error) {
+      setErrorVisible(true);
+      const timer = setTimeout(() => {
+        setErrorVisible(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
   const handleSubmitRegister = async (formData: any) => {
     try {
       const { email, password, username } = formData;
-      const result = await userRepository.addUser({ email, password, username } as UserEntity, dispatch);
+      const result = await userRepository.addUser(
+        { email, password, username } as UserEntity,
+        dispatch
+      );
       if (result) {
         router.push("/panel");
       }
@@ -35,7 +48,10 @@ export default function Home() {
   const handleSubmitLogin = async (formData: any) => {
     try {
       const { email, password } = formData;
-      const result = await userRepository.loginUser({ email, password } as UserEntity, dispatch);
+      const result = await userRepository.loginUser(
+        { email, password } as UserEntity,
+        dispatch
+      );
       if (result) {
         router.push("/panel");
       }
@@ -43,7 +59,6 @@ export default function Home() {
       console.log(err);
     }
   };
-  
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-brand-300">
@@ -63,13 +78,13 @@ export default function Home() {
               <span className="text-green-300 text-sm mr-5">Online</span>
               Dashboard
             </h2>
-            {error && (
-            <div className="flex justify-center items-center">
-              <span className="text-white bg-red-400 shadow-lg shadow-red-300 rounded-md py-2 px-4">
-                {error}
-              </span>
-            </div>
-          )}
+            {errorVisible && (
+              <div className="flex justify-center items-center">
+                <span className="text-white bg-red-400 shadow-lg shadow-red-300 rounded-md py-2 px-4">
+                  {error}
+                </span>
+              </div>
+            )}
             {showLoginForm ? (
               <LoginForm
                 setShowLoginForm={setShowLoginForm}
